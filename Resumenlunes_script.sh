34673a83850c151b99d5248659749fc39d106ad9
martinfuel123-ai <martinfuel123@gmail.com>
2026-03-30 15:01:54 +0000

    Add Resumenlunes_script.sh

---
+++ Resumenlunes_script.sh
@@ -0,0 +1,9 @@
+# Updates the package list from repositories
+sudo apt update
+
+# Upgrades installed packages to the latest versions
+sudo apt upgrade
+
+# Installs the parted tool for disk partition management
+sudo apt install parted
+
