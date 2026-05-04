5913299f0e8758a3cba8d046c435f7ebc02cf568
martinfuel123-ai <martinfuel123@gmail.com>
2026-05-04 00:42:05 +0000

    Tested aptitude command variations

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,2 +1,10 @@
 #Lists contents of the Documents directory
 ls Documents
+#Runs aptitude with a special argument (Easter egg)
+aptitude moo
+#Runs aptitude with verbose option
+aptitude -v moo
+#Runs aptitude with increased verbosity
+aptitude -vv moo
+#Runs aptitude with maximum verbosity
+aptitude -vvv moo
