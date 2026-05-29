f45df97c74c731d89e79450e6b3f90cc829fcd26
martinfuel123-ai <martinfuel123@gmail.com>
2026-05-29 15:38:40 +0000

    commit 2

--- security.zsh
+++ security.zsh
@@ -6,3 +6,17 @@ id -gn #only the main group name
 touch ~/test_grupo_heredado.txt
 ls -la ~/test_grupo_heredado.txt
 ##The group is the user's primary grou
+#View the current group
+id -gn
+echo "Grupo actual: $(id -gn)"
+
+#Create a file before newgrp
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.tx
+
+#The switch to the developers group was successfully verified using newgrp, allowing users to work with the new active group in the current session.
+#The necessary packages were also installed.
+##Create a file within the subsell
+touch ~/despues_de_newgrp.txt
+ls -la ~/despues_de_newgrp.txt
+##The group is now 'desarrolladores'
