2eba4d5f879b070e602d8c38d55756d39b187c24
martinfuel123-ai <martinfuel123@gmail.com>
2026-06-05 15:42:40 +0000

    Add stdin stdout and stderr redirection examples

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -7,4 +7,12 @@ lzl || echo "el comando lzl falló"
 echo "Hello World!" > output.txt
 cat output.txt
 echo "Goodbye!" >> output.txt
- cat output.txt
+cat output.txt
+ls -l / &> stdout_and_stderr.txt
+cat &> stdout_and_stderr.txt
+cat &>> stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+cat 1> stdout.txt 2> stderr.txt
+lzl 2> error.txt
+cat error.txt
+cat < output.txt
