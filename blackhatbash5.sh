3345397f2a88601c885da826dc0a563ca09f4a84
martinfuel123-ai <martinfuel123@gmail.com>
2026-06-05 15:29:49 +0000

    Add output redirection example

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -3,3 +3,8 @@ touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "el comando lzl falló"
+
+echo "Hello World!" > output.txt
+cat output.txt
+echo "Goodbye!" >> output.txt
+ cat output.txt
