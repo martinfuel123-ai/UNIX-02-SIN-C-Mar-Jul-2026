f7186e288a90d7522348f8f77519696210aa9d8e
martinfuel123-ai <martinfuel123@gmail.com>
2026-06-08 15:09:30 +0000

    Add argument list and argument counter

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -6,4 +6,7 @@ TARGET="${1}"
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
 
+echo "The arguments are: $@"
+echo "The total number of arguments is: $#"
+
 ping -c 3 "${TARGET}"
